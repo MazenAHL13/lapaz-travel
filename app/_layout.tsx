5f63515ebdea9c1668df1ac7a8207bf03f2d7939
Bfdiego <dayo.alba.p@gmail.com>
2025-11-03 10:58:51 -0400

    Fix: error in dark mode on the status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,20 @@
+import { useThemeStore } from "@/src/store/useThemeStore";
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
+  const theme = useThemeStore((state) => state.theme);
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="login" />
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    <>
+      <StatusBar
+          barStyle={theme === "dark" ? "light-content" : "dark-content"}
+          backgroundColor="transparent"
+          translucent
+        />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="login" />
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </> 
   );
 }
